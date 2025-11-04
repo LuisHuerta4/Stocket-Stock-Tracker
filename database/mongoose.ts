460818002f8141428f0535e29014daaf252c0074
Luis <luishuerta0518@gmail.com>
2025-11-03 20:41:32 -0800

    DB connection comments

--- database/mongoose.ts
+++ database/mongoose.ts
@@ -1,11 +1,15 @@
+// Next.js can run server code multiple times, which would normally create multiple MongoDB connections and cause performance issues. 
+// To prevent this, we store and reuse a cached database connection across hot reloads and server executions. 
+// This ensures only one connection is created and reused throughout the app lifecycle.
+
 import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 declare global {
     var mongooseCache: {
-        conn: typeof mongoose | null;
-        promise: Promise<typeof mongoose> | null;
+        conn: typeof mongoose | null;              // resolved connection
+        promise: Promise<typeof mongoose> | null;  // connection in-progress
     }
 }
 
@@ -16,20 +20,25 @@ if (!cached) {
 }
 
 export const connectToDatabase = async () => {
-    if (!MONGODB_URI) throw new Error('MONGODOB_URI is not defined in env')
+
+    if (!MONGODB_URI) throw new Error('MONGODB_URI is not defined in env');
+
 
     if (cached.conn) return cached.conn;
 
+    // prevents multiple connections from being created simultaneously
     if (!cached.promise) {
         cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
     }
 
     try {
+        // await the connection and store it in the cache
         cached.conn = await cached.promise;
     } catch (error) {
+        // reset cache if connection fails, so we can retry later
         cached.promise = null;
         throw error;
     }
 
     console.log(`MongoDB connected: ${process.env.NODE_ENV} - ${MONGODB_URI}`);
-}
+};
