b4a1ec3771127b52ff07a73c97bc843466e18daf
Luis <luishuerta0518@gmail.com>
2025-11-20 19:58:24 -0800

    Create and display watchlist table

--- app/(root)/watchlist/page.tsx
+++ app/(root)/watchlist/page.tsx
@@ -1,9 +1,11 @@
 import { Star } from 'lucide-react';
 import { searchStocks } from '@/lib/actions/finnhub.actions';
 import SearchCommand from '@/components/SearchCommand';
+import { getWatchlistWithData } from '@/lib/actions/watchlist.actions';
+import { WatchlistTable } from '@/components/WatchlistTable';
 
 const Watchlist = async () => {
-  const watchlist = []; // Actual watchlist items later
+  const watchlist = await getWatchlistWithData();
   const initialStocks = await searchStocks();
 
   // Empty state
@@ -29,7 +31,7 @@ const Watchlist = async () => {
           <h2 className="watchlist-title">Watchlist</h2>
           <SearchCommand initialStocks={initialStocks} />
         </div>
-        {/* WatchlistTable */}
+        <WatchlistTable watchlist={watchlist} />
       </div>
     </section>
   );
